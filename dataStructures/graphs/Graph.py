decc0d3c219c2b4e6126a25fde1e61807b2f48c9
srainha <srainha@berkeley.edu>
2018-06-24 13:21:16 -0700

    Update graph class

--- dataStructures/graphs/Graph.py
+++ dataStructures/graphs/Graph.py
@@ -1,14 +1,25 @@
 class Graph():
 
-	# A basic directed graph class based on adjacency list
+# A basic directed graph class based on adjacency list
 
-	def __init__(self, data=None):
-		pass
+def __init__(self, data=None):
+	pass
 
-	def addNode(self, data):
-		pass
+def addNode(self, data):
+	pass
 
-	def addEdge(self, fromNode, toNode):
-		pass
+def addEdge(self, fromNode, toNode):
+	pass
 
-	
+def deleteNode(self, data):
+	pass
+
+def deleteEdge(self, fromNode, toNode):
+	pass
+
+def containsNode(self, data):
+	pass
+
+def containsEdge(self, fromNode, toNode):
+	pass
+
